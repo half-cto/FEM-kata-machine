50f9e80d3050294245438c1d997eb685aee16df9
Juris Zemitis <juris.zemitis@gmail.com>
2023-11-20 15:13:21 +0200

    BinarySearch solved

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let startIndex = 0;
+    let endIndex = haystack.length;
+    do {
+        const middleIndex =
+            startIndex + Math.floor((endIndex - startIndex) / 2);
 
+        if (haystack[middleIndex] === needle) return true;
+        if (needle < haystack[middleIndex]) {
+            endIndex = middleIndex;
+        } else {
+            startIndex = middleIndex + 1;
+        }
+    } while (startIndex < endIndex);
+    return false;
 }
